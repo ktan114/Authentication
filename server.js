257d3f2dcdc51454c0fe356c30bbc27cb0555b27
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-05-16 16:58:49 -0400

    Initial GET route

--- server.js
+++ server.js
@@ -6,5 +6,10 @@ const server = express();
 // Middleware
 server.use(express.json());
 
+// Initial route
+server.get('/', (req, res) => {
+    res.send('api running');
+})
+
 const port = 3000;
 server.listen(port, () => console.log(`This is running on port: ${port}`))
